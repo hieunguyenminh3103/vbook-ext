2f1e390b56b8b44981555d8135afc959ea5c9274
hieunm <hieunm3103@gmail.com>
2024-10-02 05:54:04 +0700

    test: buondua

--- Buondua/src/search.js
+++ Buondua/src/search.js
@@ -1,27 +1,34 @@
-const BASE_URL = 'https://buondua.com'
+const BASE_URL = "https://buondua.com";
 
-function execute(key, start) {
-  if (!start) start = "0";
-  
-  let response = fetch(BASE_URL + "/?search=hi" + "&start=" + start);
+function execute(key, page) {
+  if (!page) page = "0";
+
+  let response = fetch(BASE_URL, {
+    method: "GET",
+    queries: {
+      search: key,
+      start: page,
+    },
+  });
+
   if (response.ok) {
     let doc = response.html();
-    var next = /\?start=(\d+)/.exec(doc.select(".pagination-next").attr("href"));
     let data = [];
-    doc.select(".main-container .main-body .blog.columns").forEach((e) => {
-      e.select(".items-row.column").forEach((item) => {
-        var cover = item.select(".item-thumb a img").attr("src");
-        var name = item.select(".item-content a").text();
-        var match = name.match(/\(\s*(\d+)\s*photos\s*\)/);
-        data.push({
-          name: name,
-          link: item.select(".item-content a").attr("href"),
-          description: match[1] + "photos",
-          cover: cover,
-          host: BASE_URL,
-        });
+    doc.select(".blog.columns > .items-row.column").forEach((e) => {
+      var name = e.select(".item-thumb img").first().attr("alt");
+      var match = name.match(/\(\s*(\d+)\s*photos\s*\)/);
+      data.push({
+        name: name,
+        description: match[1],
+        link: encodeURIComponent(
+          e.select(".page-header a").first().attr("href")
+        ).replace("%2F", "/"),
+        cover: e.select(".item-thumb img").firts().attr("src"),
+        host: BASE_URL,
       });
     });
+    var next = /\?start=(\d+)/.exec(doc.select(".pagination-next").attr("href"));
+        if (next) next = next[1];
     return Response.success(data, next);
   }
   return null;
