3b046cec7826b4c384e38761d020f04d35637850
hieunm <hieunm3103@gmail.com>
2024-10-31 06:28:47 +0700

    update home file

--- Buondua/src/home.js
+++ Buondua/src/home.js
@@ -1,12 +1,9 @@
-load('config.js');
 function execute() {
-
     return Response.success([
-        {title: "Update", input: BASE_URL, script: "gen.js"},
-        {title: "Hot", input: BASE_URL + "/hot/", script: "gen.js"},
-        {title: "Cosplay", input: BASE_URL + "/tag/cosplay-10688/", script: "gen.js"},
-        {title: "Xiuren", input: BASE_URL + "/tag/xiuren-7417", script: "gen.js"}
-        {title: "AI Generated", input: BASE_URL + "/tag/ai-generated-11406/", script: "gen.js"},
-        {title: "Random", input: BASE_URL + "/", script: ""},
+        {title: "Update", input: "https://buondua.com/", script: "gen.js"},
+        {title: "Hot", input: "https://buondua.com/hot/", script: "gen.js"},
+        {title: "Xiuren", input: "https://buondua.com/tag/7417/", script: "gen.js"},
+        {title: "Cosplay", input: "https://buondua.com/tag/10688/", script: "gen.js"},
+        {title: "AI Generated", input: "https://buondua.com/tag/11406/", script: "gen.js"},
     ]);
 }
