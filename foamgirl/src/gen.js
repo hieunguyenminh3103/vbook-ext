4d07d9148ab61a966a10aa38012972360839bda5
hieunm <hieunm3103@gmail.com>
2024-10-31 13:38:53 +0700

    foamgirl: fix lazyloading image

--- foamgirl/src/gen.js
+++ foamgirl/src/gen.js
@@ -5,20 +5,28 @@ function execute(url, page) {
     url = url.replace(BASE_URL, "");
     url = url.replace(/\/$/, "");
     if (!page) page = '1';
-    var browser = Engine.newBrowser() // Khởi tạo browser
-    browser.launch(BASE_URL + url+'/page/'+page, 5000) // Mở trang web với timeout, trả về Document object
-    var doc = browser.html() // Trả về Document object của trang web
-    browser.close() // Đóng browser khi đã xử lý xong
-    let data = [];
-    doc.select("#index_ajax_list > li").forEach(e => {
-        data.push({
-            name: e.select(".meta-title").text(),
-            link: e.select(".meta-title").attr("href"),
-            cover: e.select(".waitpic").attr("src"),
-            host: BASE_URL,
-        })
-    });
-    var next = doc.select(".next.page-numbers").first().attr("href").match(/page\/(\d+)/)
-    if (next) next = next[1]; else next = '';
-    return Response.success(data, next)
+    let response = fetch(BASE_URL + url+'/page/'+page, {
+        method: "GET"
+    })
+    if(response.ok){
+        let data = [];
+        let doc = response.html();
+        doc.select("#index_ajax_list > li").forEach(e => {
+            var insidePageUrl = e.select(".meta-title").attr("href")
+            let insidePage = fetch(insidePageUrl, {
+                method: "GET"
+            })
+            let cover = (insidePage.ok) ? insidePage.html().select("div#image_div a img").first().attr("src") : null;
+            data.push({
+                name: e.select(".meta-title").text(),
+                link: insidePageUrl,
+                cover: cover,
+                host: BASE_URL,
+            })
+        });
+        var next = doc.select(".next.page-numbers").first().attr("href").match(/page\/(\d+)/)
+        if (next) next = next[1]; else next = '';
+        return Response.success(data, next)
+    }
+    return Response.error("Something went wrong")
 }
