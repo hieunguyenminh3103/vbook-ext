d9fa5305561ae10f82247fa8a0915f0e98c350fd
hieunm <hieunm3103@gmail.com>
2024-11-03 11:45:17 +0700

    update

--- foamgirl/src/chap.js
+++ foamgirl/src/chap.js
@@ -1,6 +1,7 @@
 load('config.js');
 function execute(url) {
-    let response= fetch(BASE_URL + url);
+    let id = url.match(/(\d+(?:_\d+)?)\.html/)[1]
+    let response= fetch(BASE_URL + '/' + id + '.html');
     if (response.ok) {
         let doc = response.html();
         let data = [];
